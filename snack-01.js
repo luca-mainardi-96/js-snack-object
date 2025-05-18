9daef95482c5d81dc69dc8f25aa8b568d71fb8fb
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-18 20:10:55 +0200

    inizio

---
+++ snack-01.js
@@ -0,0 +1,59 @@
+const auto = [
+    {
+        marca: 'Toyota',
+        modello: 'Corolla',
+        alimentazione: 'Benzina'
+    },
+    {
+        marca: 'Ford',
+        modello: 'Focus',
+        alimentazione: 'Diesel'
+    },
+    {
+        marca: 'Opel',
+        modello: 'Astra',
+        alimentazione: 'GPL'
+    },
+    {
+        marca: 'Tesla',
+        modello: 'Model 3',
+        alimentazione: 'Elettrico'
+    },
+    {
+        marca: 'Volkswagen',
+        modello: 'Golf',
+        alimentazione: 'Metano'
+    },
+    {
+        marca: 'BMW',
+        modello: 'Serie 3',
+        alimentazione: 'Benzina'
+    },
+    {
+        marca: 'Peugeot',
+        modello: '208',
+        alimentazione: 'Diesel'
+    },
+    {
+        marca: 'Honda',
+        modello: 'Civic',
+        alimentazione: 'Benzina'
+    },
+    {
+        marca: 'Nissan',
+        modello: 'Leaf',
+        alimentazione: 'Elettrico'
+    },
+    {
+        marca: 'Renault',
+        modello: 'Clio',
+        alimentazione: 'Benzina'
+    }
+
+]
+
+console.log(auto)
+
+const autoBenzina = []
+const autoDiesel = []
+const restoAuto = []
