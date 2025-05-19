6b18e80614e30a6b9a4f33f5f9643a604c941831
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-19 21:55:06 +0200

    correzione esercizio 2

--- snack-02.js
+++ snack-02.js
@@ -2,17 +2,32 @@ const parole = ['matita', 'temperino', 'MODEM']
 console.log(parole)
 
 //creare un array con le parole tutte con la prima lettera maiuscola
-parole[2] = parole[2].toLowerCase()
-const parole2 = []
+// parole[2] = parole[2].toLowerCase()
+// const parole2 = []
 
- for (i=0; i<parole.length; i++){
-    const lettere = parole[i].split('')
-    lettere[0] = lettere[0].toUpperCase()
-    const parola2 = lettere.join('')
-    console.log(parola2)
-    parole2.push(parola2)
+//  for (i=0; i<parole.length; i++){
+//     const lettere = parole[i].split('')
+//     lettere[0] = lettere[0].toUpperCase()
+//     const parola2 = lettere.join('')
+//     console.log(parola2)
+//     parole2.push(parola2)
 
- }
+//  }
 
 
- console.log(parole2)
+//  console.log(parole2)
+
+const paroleFormattate = []
+
+for (let i=0; i<parole.length; i++){
+   const parola = parole[i]
+   const firstChar = parola.charAt(0).toLocaleUpperCase()
+   // firstChar = firstChar.toLocaleUpperCase()
+   const restoParola = parola.slice(1).toLowerCase()
+   // restoParola = restoParola.toLowerCase()
+
+  paroleFormattate.push(firstChar+restoParola)
+
+  console.log(paroleFormattate)
+
+}
